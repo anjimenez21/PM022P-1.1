5a7dd8fd1de4029a65cfeb0656dda8296ad0e686
anjimenez21 <81935849+anjimenez21@users.noreply.github.com>
2023-11-08 22:19:50 -0600

    Creación de proyecto

---
+++ Tarea1.3/Tarea1.3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tarea1._3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
